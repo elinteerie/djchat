60ed9fdfb900aa42f331127260b4e804480e73ed
elinteerie <elinteerie@gmail.com>
2023-08-02 10:31:12 +0100

    added

--- chat/chat/urls.py
+++ chat/chat/urls.py
@@ -22,6 +22,10 @@ from server.views import ServerListViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register("api/server/select", ServerListViewSet)
@@ -31,7 +35,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/docs/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/schema/ui/', SpectacularSwaggerView.as_view()),
-    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ] + router.urls
 
